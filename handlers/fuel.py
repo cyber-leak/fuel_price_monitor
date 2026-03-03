84d6d25d4466c33f7854ee58caa4cc71b28f3a6f
Daysi-ibs <ilya.bazarov72@gmail.com>
2026-03-03 16:42:54 +0200

    docs: finalize fuel handler logic

--- handlers/fuel.py
+++ handlers/fuel.py
@@ -4,6 +4,5 @@
 def register_fuel_handlers(bot):
     @bot.message_handler(commands=['fuel'])
     def send_fuel_prices(message):
-        user_name = message.from_user.________
+        user_name = message.from_user.first_name
         bot.send_message(message.chat.id, f"{user_name}, заправки WOG: 55 грн")
-
